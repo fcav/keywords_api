b2812c9fea319b518560c7b462aaabd8e2d58dc6
sean.blumenfeld <sean.blumenfeld@essencedigital.com>
2015-02-28 15:16:47 -0500

    config file

---
+++ keywords_api/config.py
@@ -0,0 +1,23 @@
+#These will be in the function
+PAGE_SIZE = 10
+
+SELECTOR = {
+    'searchParameters': [{
+        'xsi_type': 'RelatedToQuerySearchParameter',
+        #Variable input with function here
+        'queries': ['jumper']
+    },
+    {
+        #Language setting from variable here
+        'xsi_type': 'LanguageSearchParameter',
+        'languages': [{'id': '1000'}]
+    }],
+    'ideaType': 'KEYWORD',
+    'requestType': 'IDEAS',
+    'requestedAttributeTypes': ['KEYWORD_TEXT',
+                                'SEARCH_VOLUME',
+                                'AVERAGE_CPC',
+                                'COMPETITION',],
+    'paging': {'startIndex': '0',
+               'numberResults': str(PAGE_SIZE)}
+}
