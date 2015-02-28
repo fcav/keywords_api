40051aaa72b557a697f215858e12452faa9d5f73
sean.blumenfeld <sean.blumenfeld@essencedigital.com>
2015-02-28 14:10:43 -0500

    rename

--- bin/ApiConnector.py
+++
@@ -1,9 +0,0 @@
-import sys
-from googleads.adwords import AdWordsClient
-
-WORKING_DIR = '/home/seanblumenfeld/projects/keywords_api/bin/'
-YAML_FILE = WORKING_DIR + 'googleads.yaml'
-sys.path.append(WORKING_DIR)
-
-client = AdWordsClient.LoadFromStorage(path=yaml_file)
-targeting_idea_service = client.GetService('TargetingIdeaService')
