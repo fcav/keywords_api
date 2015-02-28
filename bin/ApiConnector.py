18866e9d156d0788730b28f6c4b3939f2bc52437
sean.blumenfeld <sean.blumenfeld@essencedigital.com>
2015-02-28 14:05:26 -0500

    add test files

--- bin/ApiConnector.py
+++ bin/ApiConnector.py
@@ -1,10 +1,9 @@
 import sys
-
-working_dir = '/Users/youcefkadri/Downloads/googleads-python-lib-master/'
-yaml_file = working_dir + 'googleads.yaml'
-
-sys.path.append(working_dir)
 from googleads.adwords import AdWordsClient
+
+WORKING_DIR = '/home/seanblumenfeld/projects/keywords_api/bin/'
+YAML_FILE = WORKING_DIR + 'googleads.yaml'
+sys.path.append(WORKING_DIR)
 
 client = AdWordsClient.LoadFromStorage(path=yaml_file)
 targeting_idea_service = client.GetService('TargetingIdeaService')
