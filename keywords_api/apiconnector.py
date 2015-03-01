846c70d0a40eccbce44070d607f0902461aa701a
sean.blumenfeld <sean.blumenfeld@essencedigital.com>
2015-02-28 20:45:37 -0500

    add multiple service cons

--- keywords_api/apiconnector.py
+++ keywords_api/apiconnector.py
@@ -11,9 +11,12 @@ from keywords_api.config import SELECTOR, DATA_DIR, YAML_FILE
 
 class ApiConnector(object):
 
+    def __init__(self, service_name='TargetingIdeaService'):
+        self.service_name = service_name
+
     def getIdeaService(self):
         self.client = AdWordsClient.LoadFromStorage(path=YAML_FILE)
-        self.service = self.client.GetService('TargetingIdeaService')
+        self.service = self.client.GetService(self.service_name)
         return self.service
 
 
@@ -29,13 +32,12 @@ class IdeaSelector(object):
     def _get_language(self, language):
         return 1000
 
-    def buildSelector(self, keyword, language, results_per_request, location):
-        pdb.set_trace()
+    def buildSelector(self, language='English', location='en_US', page_size=10):
         self.selector = SELECTOR
         language_code = str(self._get_language(language))
-        keyword_param = {'xsi_type': 'RelatedToQuerySearchParameter', 'queries': [keyword]}
+        keyword_param = {'xsi_type': 'RelatedToQuerySearchParameter', 'queries': [self.keyword]}
         language_param = {'xsi_type': 'LanguageSearchParameter','languages': [{'id': language_code}]}
-        paging_param =  {'startIndex': '0','numberResults': str(results_per_request)}
+        paging_param =  {'startIndex': '0','numberResults': str(page_size)}
         self.selector['searchParameters'] = [keyword_param, language_param]
         self.selector['localeCode'] = location
         self.selector['paging'] = paging_param
@@ -52,6 +54,7 @@ class IdeaSelector(object):
             }
         """
         page = self.service.get(self.selector)
+
         ideas = page.entries
         clean_ideas = []
         for idea in ideas:
@@ -68,8 +71,8 @@ class IdeaSelector(object):
 
 class IdeasIterator():
 
-    def __init__(self, results_per_request=10, iterations=5, language='English', location='UK'):
-        self.results_per_request = results_per_request
+    def __init__(self, page_size=10, iterations=5, language='English', location='UK'):
+        self.page_size = page_size
         self.iterations = iterations
         self.language = language
         self.location = location
@@ -82,8 +85,8 @@ class IdeasIterator():
         for i in range(1, self.iterations+1):
             next_keywords = []
             for k in this_keywords:
-                new_selector = IdeaSelector(self.selector, keywords)
-                new_selector.buildSelector(k, self.language, self.results_per_request, self.location)
+                new_selector = IdeaSelector(self.selector, k)
+                new_selector.buildSelector(self.language, self.location, self.page_size)
                 this_ideas = new_selector.get_ideas()
                 next_keywords += [x['keyword'] for x in this_ideas.values()]
                 self.write_in_csv(this_ideas, i)
@@ -102,11 +105,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-k', "--keywords", help="The keywords you want to start with", nargs='+')
     parser.add_argument('-i', "--iterations", default = 5, help="Number of iteration. If not given it will defaults to 5")
-    parser.add_argument("-r", "--results", default = 10, help="Number of results per iteration. If not given it will default to 10")
+    parser.add_argument("-r", "--page_size", default = 10, help="Number of results per iteration. If not given it will default to 10")
     parser.add_argument("-ln", "--language", default = 'English', help="Language. If not entered it will default to English")
     parser.add_argument("-lc", "--location", default = 'en_US', help="Location. If not entered it will default to UK")
     args = parser.parse_args()
 
 
-    ideas = IdeasIterator(args.results, args.iterations, args.language, args.location)
+    ideas = IdeasIterator(args.page_size, args.iterations, args.language, args.location)
     ideas.run(args.keywords)
