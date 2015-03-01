0a04edc4f4113c0ca7a6e7032d13519aa0fd82d1
sean.blumenfeld <sean.blumenfeld@essencedigital.com>
2015-03-01 14:38:02 -0500

    add test

--- tests/tests.py
+++ tests/tests.py
@@ -4,6 +4,7 @@ import unittest
 from keywords_api.apiconnector import ApiConnector, IdeaSelector, IdeasIterator, LocationSelector, NonExistantCode
 from keywords_api.config import SELECTOR
 import csv
+import mock
 
 class TestApiConnector(unittest.TestCase):
 
@@ -68,23 +69,32 @@ class TestIterator(unittest.TestCase):
 
     def setUp(self):
         self.test_ideas = {'keyword for unittest': [{'RANK': 1, 'AVERAGE_CPC': '3139613', 'SEARCH_VOLUME': '260', 'KEYWORD_TEXT': 'testing python', 'COMPETITION': '0.0361320208454'}, {'RANK': 2, 'AVERAGE_CPC': '1300835', 'SEARCH_VOLUME': '30', 'KEYWORD_TEXT': 'testing python code', 'COMPETITION': '0.0454545454545'}, {'RANK': 3, 'AVERAGE_CPC': None, 'SEARCH_VOLUME': '70', 'KEYWORD_TEXT': 'python test module', 'COMPETITION': '0.020979020979'}, {'RANK': 4, 'AVERAGE_CPC': '910906', 'SEARCH_VOLUME': '70', 'KEYWORD_TEXT': 'python test code', 'COMPETITION': '0.0314253647587'}, {'RANK': 5, 'AVERAGE_CPC': '2689673', 'SEARCH_VOLUME': '10', 'KEYWORD_TEXT': 'python class module', 'COMPETITION': '0.0179728317659'}, {'RANK': 6, 'AVERAGE_CPC': None, 'SEARCH_VOLUME': '30', 'KEYWORD_TEXT': 'python code test', 'COMPETITION': '0.0642424242424'}, {'RANK': 7, 'AVERAGE_CPC': None, 'SEARCH_VOLUME': '10', 'KEYWORD_TEXT': 'tests python', 'COMPETITION': '0.0136363636364'}, {'RANK': 8, 'AVERAGE_CPC': None, 'SEARCH_VOLUME': '20', 'KEYWORD_TEXT': 'what is function in python', 'COMPETITION': '0.0'}, {'RANK': 9, 'AVERAGE_CPC': None, 'SEARCH_VOLUME': '30', 'KEYWORD_TEXT': 'programming unit test', 'COMPETITION': '0.0534435261708'}, {'RANK': 10, 'AVERAGE_CPC': None, 'SEARCH_VOLUME': '10', 'KEYWORD_TEXT': 'python code testing', 'COMPETITION': '0.0661157024793'}]}
-        self.num_of_test_ideas = len(self.test_ideas['keyword for unittest'])
+        self.test_keywords = ['keyword for unittest']
+        #self.num_of_test_ideas = len(self.test_ideas['keyword for unittest'])
         self.test_headers = ['RANK', 'AVERAGE_CPC', 'SEARCH_VOLUME', 'KEYWORD_TEXT', 'COMPETITION', 'ITERATION', 'SEED_KEYWORD']
-        self.iter = IdeasIterator(self.test_ideas)
+        self.iter = IdeasIterator(self.test_keywords)
         self.testfile = self.iter.output_file
         os.remove(self.testfile) if os.path.exists(self.testfile) else None
 
     def test_file_is_created(self):
         self.assertFalse(os.path.exists(self.testfile))
-        self.iter.append_to_csv(1)
+        self.iter.append_to_csv(self.test_ideas, 1)
         self.assertTrue(os.path.exists(self.testfile))
 
     def test_writes_headers_to_csv(self):
-        self.iter.append_to_csv(1)
+        pdb.set_trace()
+        self.iter.append_to_csv(self.test_ideas, 1)
         with open(self.testfile) as f:
             reader = csv.reader(f)
             headers = reader.next()
             self.assertItemsEqual(headers, self.test_headers)
+
+    def test_run_calls_append_to_csv(self):
+        self.iter.append_to_csv = mock.Mock()
+        self.iter.run()
+        pdb.set_trace()
+        self.iter.append_to_csv.assert_called()
+
 
 if __name__ == '__main__':
     api_con_suite = unittest.TestLoader().loadTestsFromTestCase(TestApiConnector)
