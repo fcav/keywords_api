9fcba2e870f0de46461495a990ae2e9b7477e1ed
Flaminia <flaminia.cavallo@essencedigital.com>
2015-03-01 00:40:25 -0500

    fixed default language

--- keywords_api/apiconnector.py
+++ keywords_api/apiconnector.py
@@ -3,6 +3,7 @@ import pdb
 import csv
 import argparse
 import sys
+import os
 from googleads.adwords import AdWordsClient
 from keywords_api.config import SELECTOR, DATA_DIR, YAML_FILE, LANGUAGE, LOCATION_SELECTOR, LANGUAGE_SELECTOR
 
@@ -27,9 +28,6 @@ class LanguageSelector():
 
     def get_code(self):
         languages = self.service.getLanguageCriterion()
-        pdb.set_trace()
-        if not languages:
-            raise
         return languages
 
 class LocationSelector():
@@ -46,7 +44,7 @@ class LocationSelector():
         self.buildselector(location)
         a = self.service.get(self.selector)
         if not a:
-            raise NonExistantCode('The location taht you have given does not exist')
+            raise NonExistantCode('The location that you have given does not exist')
         return [x.location.id for x in a][0]
 
 
@@ -60,11 +58,11 @@ class IdeaSelector(object):
         else:
             raise TypeError('keyword must be a string')
 
-    def buildSelector(self, language='en_US', location=2826, page_size=10):
+    def buildSelector(self, language='1000', location=2826, page_size=10):
         self.page_size = page_size
         self.selector = SELECTOR
         keyword_param = {'xsi_type': 'RelatedToQuerySearchParameter', 'queries': [self.keyword]}
-        language_param = {'xsi_type': 'LanguageSearchParameter','languages': [{'id': '1004'}]}
+        language_param = {'xsi_type': 'LanguageSearchParameter','languages': [{'id': language}]}
         location_param = {'xsi_type': 'LocationSearchParameter','locations': [{'id': location}]}
         paging_param =  {'startIndex': '0','numberResults': str(page_size)}
         self.selector['searchParameters'] = [keyword_param, location_param, language_param]
@@ -158,7 +156,7 @@ if __name__ == '__main__':
         sys.exit()
 
     locationcode = LocationSelector().get_code(args.location)
-    languagecode = LANGUAGE.get(args.language, None)
+    languagecode = str(LANGUAGE[args.language])
 
-    ideas = IdeasIterator(args.page_size, args.iterations, args.language, locationcode, args.keywords)
+    ideas = IdeasIterator(args.keywords, args.page_size, args.iterations, languagecode, locationcode)
     ideas.run()
